66bc17289e71d40d56653b67366762410ed30abc
Carendoh <saschaslob@gmail.com>
2021-06-06 00:04:56 +0300

    Added both neural layer and neural network classes.

---
+++ LR4/RecognitionTest/NeuralLayer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RecognitionTest
+{
+    public class NeuralLayer<TResult> where TResult : struct, IComparable, IConvertible, IComparable<TResult>, IEquatable<TResult>
+    {
+        public NeuralLayer()
+            :this(new List<Neuron<TResult>>())
+        {
+        }
+
+        public NeuralLayer(List<Neuron<TResult>> neurons)
+        {
+            Neurons = neurons;
+        }
+
+        private List<Neuron<TResult>> Neurons { get; }
+
+        public Neuron<TResult> this[int i] => Neurons[i];
+    }
+}
